8465652c065855ce1082fef7cbbddea73d990bf8
Kerim Nezo <kerim.nezo.20@size.ba>
2022-11-27 13:28:01 +0100

    Commit templates foldera i prvog htmla

--- market.py
+++ market.py
@@ -1,13 +1,8 @@
-from flask import Flask #import-amo flask instancu iz flask paketa
+from flask import Flask, render_template #import-amo flask instancu iz flask paketa(and whole lot more)
 app = Flask(__name__) #incijaliziramo instancu Flaska sa argumentom
 # __name__ (built-in var, referira na lokalni py file s kojim radimo)
 
 @app.route("/") #dekorator ide liniju prije funkcije i govori nam na kojem url-u će se 
 # prikazati html kod(funkcija) "/" je homepage
-def hello_world():
-    return "<h1>Hello World</h1>"
-
-@app.route("/about/<username>") #dinamic routes, rute koje primaju varijable koje postaju dio linka
-# upamti kako se ponasaju
-def about(username): # ova funkcija handle-a sve requeste koji dođu na ovu rutu koja je u dekoratoru 
-    return f"<h1>This is the about page of {username}</h1>"
+def home_page():
+    return render_template('home.html') # po konvenciji je da se stvori templates folder za sav html kod
