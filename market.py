40cd84d37d60839cb521d6cca7d9b0586e4fdd17
Kerim Nezo <kerim.nezo.20@size.ba>
2022-11-27 13:52:24 +0100

    Styling i Templates

--- market.py
+++ market.py
@@ -3,6 +3,8 @@ app = Flask(__name__) #incijaliziramo instancu Flaska sa argumentom
 # __name__ (built-in var, referira na lokalni py file s kojim radimo)
 
 @app.route("/") #dekorator ide liniju prije funkcije i govori nam na kojem url-u će se 
+@app.route("/home") #funckija dole brine o dva url. Ima smisla da bude home page u oba slucaja
 # prikazati html kod(funkcija) "/" je homepage
 def home_page():
     return render_template('home.html') # po konvenciji je da se stvori templates folder za sav html kod
+
