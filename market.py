97b8d695825732b898b5d54e67528cf6ddf68edc
Kerim Nezo <kerim.nezo.20@size.ba>
2022-11-27 21:13:40 +0100

    Rad sa modelima i bazom (sqlalchemy)

--- market.py
+++ market.py
@@ -1,6 +1,20 @@
-from flask import Flask, render_template #import-amo flask instancu iz flask paketa(and whole lot more)
-app = Flask(__name__) #incijaliziramo instancu Flaska sa argumentom
-# __name__ (built-in var, referira na lokalni py file s kojim radimo)
+from flask import Flask, render_template 
+from flask_sqlalchemy import SQLAlchemy 
+app = Flask(__name__) 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+db = SQLAlchemy(app) 
+
+class Item(db.Model): #buduci da je Item klasa izvedena iz klase Model, to bazi govori da je Item klasa model
+    id = db.Column(db.Integer, primary_key=True) #treba pk
+    name = db.Column(db.String(length=30), nullable=False, unique=True) #name je kolona u bazi, koja je tipa string 
+    # ima max 30charactera, ne smije biti null, te je ime itema unique
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)#barcode je string jer se s njim nece
+    # raditi nikakve kalkulacije, vec samo postoji
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f'Item {self.name}'
 
 @app.route("/") #dekorator ide liniju prije funkcije i govori nam na kojem url-u će se 
 @app.route("/home") #funckija dole brine o dva url. Ima smisla da bude home page u oba slucaja
@@ -10,10 +24,6 @@ def home_page(): # check navbar in base.html za imena funkcija
 
 @app.route("/market")
 def market_page():
-    items = [ #lista sa 3 dictionarija sa istim keyevima(varijablama) razlicitim vrijednostima
-        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
-    ]
+    items = Item.query.all() #svi itemi spaseni u bazi
     return render_template("market.html", items=items) #Ovaj podatak dobavljamo putem varijable
     # {{ item_name }}
