67c875efbfb0409aa22d184e23d5c5fad92375d3
Kerim Nezo <kerim.nezo.20@size.ba>
2022-11-29 00:18:44 +0100

    Flask Validation, ne priakzuje u cmd ali radi validacija

--- market/forms.py
+++ market/forms.py
@@ -1,10 +1,17 @@
 from flask_wtf import FlaskForm # klasa pomocu koje mozemo praviti forme od klasa
 from wtforms import StringField, PasswordField, SubmitField # pomocu ovih klasa koje dolaze iz FlaskForm, kreiramo atribute klase da budu kao input dijelovi forme
 # good stuff fr fr
+from wtforms.validators import Length, EqualTo, Email, DataRequired #Importamo 
+#built-in klasu Length koja brine i duzini atributa u formi
+#EqualTo koji prima parametar i provjerava je li parametar jednak sa vrijednostcu polja u kojem je EqualTo
+#Email provjerava je li value polja email
+#DataRequired provjerava da li u polju uopste ima ista napisano
+
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='User Name:')
-    email_address = StringField(label='Email Address:')
-    password1 = PasswordField(label='Password:') # posebno polje od wtf za passworde
-    password2 = PasswordField(label='Confirm Password:') # za "confirm password" ono da budu isti
+    # ako hocemo vise validacija po polju, moramo praviti listu validacija
+    username = StringField(label='User Name:', validators=[Length(min=2,max=30), DataRequired()])
+    email_address = StringField(label='Email Address:', validators=[Email(), DataRequired()])
+    password1 = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()]) # posebno polje od wtf za passworde
+    password2 = PasswordField(label='Confirm Password:', validators=[EqualTo('password1'), DataRequired()])
     submit = SubmitField(label='Create Account') #za submit dugme
--- market/routes.py
+++ market/routes.py
@@ -1,9 +1,10 @@
 from market import app
-from flask import render_template
-from market.models import Item
+from flask import render_template, redirect, url_for #redirect sluzi da usmjeri korisnika na drugu stranicu jer je now logged
+from market.models import Item, User
 from market.forms import RegisterForm
+from market import db #db mogu importovati direktno iz marketa jer se db nalazi u dundur fileu __init__
 
-@app.route("/") #dekorator ide liniju prije funkcije i govori nam na kojem url-u će se 
+@app.route("/") #dekorator ide liniju prije funkcije i govori nam na kojem url-u će se prikazati 
 @app.route("/home") #funckija dole brine o dva url. Ima smisla da bude home page u oba slucaja
 # prikazati html kod(funkcija) "/" je homepage
 def home_page(): # check navbar in base.html za imena funkcija
@@ -15,10 +16,25 @@ def market_page():
     return render_template("market.html", items=items) #Ovaj podatak dobavljamo putem varijable
     # {{ item_name }}
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])# ovu listu ubacujemo da nasa ruta moze handlat post i get metode
 def register_page():
-    form = RegisterForm()
+    form = RegisterForm() #klasa iz forms.py
+    if form.validate_on_submit(): # what happens when user submits a 'create account' button
+        #ovaj if postoji da provjer 
+        # 1. da je korisnik kliknuo dugme
+        # 2. da je korisnik unio ispravne podatke(radimo to pomocu validacija)
+        user_to_create = User(username=form.username.data,
+                              email_address=form.email_address.data,
+                              password_hash=form.password1.data)
+        db.session.add(user_to_create) #dodaje instancu klase koja je primila podatke iz forme
+        db.session.commit() # ovo su naredbe koje smo prije rucno u cmd-u unosili da korisnike unesemo
+        return redirect(url_for('market_page')) #legit kao u base.html u navbaru objasnjenje, da ne bude hard codirano
+    #ako neka validacije padne(nije uneseno kako smo napisalo s validacijama) fail ce biti registrovan ovdje
+    if form.errors != {}: # ako ima(jer nije prazan) errora od validacije (ljevicaste jer je form.errors dictionary)
+        for err_msg in form.errors.values():
+            print(f'There was an error with creating a user: {err_msg}')
+            # meni ne pokazuje errore u cmd-u idk why, ali validacija radi tj ne spasava lose usere u bazu, sto je dobro
     return render_template("register.html", form=form)
 # za formu nam treba key, koji se nalazi u __init__,py on nam tehnicki daje layer
 # sigurnosti za forme, jer ipak s formama dajemo da ubacuju real podatke u nasu bazu
-# which we need to secure 
+# which we need to secure
